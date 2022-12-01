dcbcf3a557edf1a4a2fb3b0609f53f93713451bd
duytdph21921 <101155843+duytdph21921@users.noreply.github.com>
2022-12-02 01:58:07 +0700

    Add project files.

---
+++ B_BUS/ViewModel/KhachHangViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B_BUS.ViewModel
+{
+    public class KhachHangViewModel
+    {
+        public Guid id { get; set; }
+        public string? Ma { get; set; }
+        public string? Ten { get; set; }
+        public string? TenDem { get; set; }
+        public string? Ho { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string? sdt { get; set; }
+       
+        public string? ThanhPho { get; set; }
+        public string? QuocGia { get; set; }
+        public int? trangthai { get; set; }
+        // hiển thị from
+        public string HTTT { get; set; }
+        public string HoVaTen { get; set; }
+        public string? DiaChi { get; set; }
+
+    }
+}
